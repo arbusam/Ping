b69a5abef01ac9f004525d93ef0c12673ac08871
arbusam <arhan.busam@gmail.com>
2024-11-26 19:09:58 +1100

    Ball bounces off things and scores points

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,41 +1,43 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float verticalSpeed = 5f;
     [SerializeField] private float horizontalSpeed = 5f;
-
-    [SerializeField] private CircleCollider2D circleCollider2D;
+    [SerializeField] private TextMeshProUGUI scoreText;
 
     bool goingleft = true;
 
+    int score = 0;
+
     void Start()
     {
-        
+        scoreText.text = "Score: " + score;
     }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0, horizontalSpeed, 0) * Time.deltaTime;
+            transform.position += new Vector3(0, verticalSpeed, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.UpArrow)) {
-            transform.position += new Vector3(0, horizontalSpeed, 0) * Time.deltaTime;
+            transform.position += new Vector3(0, verticalSpeed, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(0, -horizontalSpeed, 0) * Time.deltaTime;
+            transform.position += new Vector3(0, -verticalSpeed, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.DownArrow)) {
-            transform.position += new Vector3(0, -horizontalSpeed, 0) * Time.deltaTime;
+            transform.position += new Vector3(0, -verticalSpeed, 0) * Time.deltaTime;
         }
         if (goingleft) {
-            transform.position += new Vector3(-verticalSpeed, 0, 0) * Time.deltaTime;
+            transform.position += new Vector3(-horizontalSpeed, 0, 0) * Time.deltaTime;
         }
         else {
-            transform.position += new Vector3(verticalSpeed, 0, 0) * Time.deltaTime;
+            transform.position += new Vector3(horizontalSpeed, 0, 0) * Time.deltaTime;
         }
     }
 
@@ -48,6 +50,13 @@ public class PlayerMovement : MonoBehaviour
         else if (collision.gameObject.tag == "Wall")
         {
             goingleft = true;
+        }
+        else if (collision.gameObject.tag == "Goal")
+        {
+            transform.position = new Vector3(0, 0, 0); 
+            goingleft = true;
+            score++;
+            scoreText.text = "Score: " + score;
         }
     }
 }
