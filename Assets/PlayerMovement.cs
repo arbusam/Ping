1da4e4057ef7e06a088b0585b0796f8e4bced21a
Arhan Busam <arhan.busam@gmail.com>
2024-11-26 18:17:47 +1100

    Enhance player movement with adjustable speed and collision detection

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,28 +3,51 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float verticalSpeed = 5f;
+    [SerializeField] private float horizontalSpeed = 5f;
+
+    [SerializeField] private CircleCollider2D circleCollider2D;
+
+    bool goingleft = true;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0, 1, 0);
+            transform.position += new Vector3(0, horizontalSpeed, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.UpArrow)) {
-            transform.position += new Vector3(0, 1, 0);
+            transform.position += new Vector3(0, horizontalSpeed, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(0, -1, 0);
+            transform.position += new Vector3(0, -horizontalSpeed, 0) * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.DownArrow)) {
-            transform.position += new Vector3(0, -1, 0);
+            transform.position += new Vector3(0, -horizontalSpeed, 0) * Time.deltaTime;
+        }
+        if (goingleft) {
+            transform.position += new Vector3(-verticalSpeed, 0, 0) * Time.deltaTime;
+        }
+        else {
+            transform.position += new Vector3(verticalSpeed, 0, 0) * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            goingleft = false;
+        }
+        else if (collision.gameObject.tag == "Wall")
+        {
+            goingleft = true;
         }
     }
 }
